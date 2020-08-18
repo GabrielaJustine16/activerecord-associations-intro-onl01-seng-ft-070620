cac5c11fc36c915b92fda54d64258f949e6fe9af
Gabriela johnson <johnsongabriela12@gmail.com>
2020-08-18 18:58:37 +0000

    Automatically backed up by Learn

--- db/migrate/02_create_genres.rb
+++ db/migrate/02_create_genres.rb
@@ -1,11 +1,9 @@
-class CreateSongs < ActiveRecord::Migration[4.2]
+class CreateGenres < ActiveRecord::Migration[4.2]
 
     def change
-        create_table :songs do |t|
+        create_table :genres do |t|
             t.string :name
-            t.integer :artist_id
-            t.integer :genre_id
         end
-    end 
+    end
     
 end
